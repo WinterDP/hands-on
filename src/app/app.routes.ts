0fbb53b747ce19c50abd1fbf27a1860b1cf36935
Lucas Oliveira <d2022004860@unifei.edu.br>
2023-09-16 00:38:59 -0300

    [FEAT] - hello world table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
+import { HelloWordComponent } from './hello-word/hello-word.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'helloword',
+        component: HelloWordComponent,
+        pathMatch: 'full',
+    },
+
+];
