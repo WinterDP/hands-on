80e15e94734bffc633797e8439d63af2a9d9917a
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-09-18 07:18:59 -0300

    [FEAT] - Hello world works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HelloWorldComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
