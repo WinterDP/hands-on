f8d8f537f1d82fe4790fedf5e89f6e1678843643
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-09-21 22:45:09 -0300

    [FEAT] - add footer library

---
+++ libs/ui/footer-lib/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FooterLibComponent } from './footer-lib/footer-lib.component';
+
+export const footerLibRoutes: Route[] = [
+  { path: '', component: FooterLibComponent },
+];
