47c3246d24a4f7ca625c0d773c9550e2addc0e27
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-10-02 17:10:59 -0300

    [IMP] - update master page

--- libs/ui/master-page/src/lib/master-page/master-page.component.ts
+++ libs/ui/master-page/src/lib/master-page/master-page.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from 'libs/ui/master-page/src/lib/footer/src/lib/footer/footer.component';
 import { NavigationComponent } from 'libs/ui/master-page/src/lib//navigation/src/lib/navigation/navigation.component';
 import { CardComponent } from '../Card/card.component';
+import { MaterialModule } from 'src/app/Material/material.module';
 
 @Component({
   selector: 'lib-master-page',
   standalone: true,
-  imports: [CommonModule, FooterComponent, NavigationComponent, CardComponent],
+  imports: [CommonModule, FooterComponent, NavigationComponent, CardComponent, MaterialModule],
   templateUrl: './master-page.component.html',
   styleUrls: ['./master-page.component.css'],
 })
--- libs/ui/master-page/src/lib/navigation/src/lib/navigation/navigation.component.ts
+++ libs/ui/master-page/src/lib/navigation/src/lib/navigation/navigation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MsalLoginComponent } from 'src/app/msal-login/msal-login.component';
+import { MaterialModule } from 'src/app/Material/material.module';
 
 @Component({
   selector: 'lib-navigation',
   standalone: true,
-  imports: [CommonModule, MsalLoginComponent],
+  imports: [CommonModule, MsalLoginComponent, MaterialModule],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
