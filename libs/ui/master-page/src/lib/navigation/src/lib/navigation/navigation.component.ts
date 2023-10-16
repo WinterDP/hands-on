320adc324fda2b88f8d8c350eb46060bd1b55529
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-10-15 22:42:07 -0300

    [IMP] clean msal code

--- libs/ui/master-page/src/lib/navigation/src/lib/navigation/navigation.component.ts
+++ libs/ui/master-page/src/lib/navigation/src/lib/navigation/navigation.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MsalLoginComponent } from 'src/app/msal-login/msal-login.component';
 import { MaterialModule } from 'src/app/Material/material.module';
 
 @Component({
   selector: 'lib-navigation',
   standalone: true,
-  imports: [CommonModule, MsalLoginComponent, MaterialModule],
+  imports: [CommonModule, MaterialModule],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
