23c6cf454af4c55116fa37212ef6ba6b3a5e5563
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-10-16 22:44:50 -0300

    [FEAT] - finish login page design
    - use tailwind flowbite templates to design the page

--- libs/ui/master-page/src/lib/landing-page/src/lib/landing-page/landing-page.component.ts
+++ libs/ui/master-page/src/lib/landing-page/src/lib/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'lib-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
