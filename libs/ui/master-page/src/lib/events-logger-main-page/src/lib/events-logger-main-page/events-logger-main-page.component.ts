607fe8881a038abd7b488a64b5ea2e68fe08bb88
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-10-26 00:54:13 -0300

    [WIP] - add entry component

--- libs/ui/master-page/src/lib/events-logger-main-page/src/lib/events-logger-main-page/events-logger-main-page.component.ts
+++ libs/ui/master-page/src/lib/events-logger-main-page/src/lib/events-logger-main-page/events-logger-main-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { EntryComponent } from 'src/app/Entry/entry.component';
 
 @Component({
   selector: 'lib-events-logger-main-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, EntryComponent],
   templateUrl: './events-logger-main-page.component.html',
   styleUrls: ['./events-logger-main-page.component.css'],
 })
+
 export class EventsLoggerMainPageComponent {}
