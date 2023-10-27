bde927285bfd78e47507e30aa2034b733fcca0e5
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-10-27 08:54:00 -0300

    [WIP] - add entry infinity scroll coponent n service

---
+++ src/app/EntryInfiniteScroll/EntryInfiniteScroll/entry-infinite-scroll.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-entry-infinite-scroll',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './entry-infinite-scroll.component.html',
+  styleUrls: ['./entry-infinite-scroll.component.css'],
+})
+export class EntryInfiniteScrollComponent {}
