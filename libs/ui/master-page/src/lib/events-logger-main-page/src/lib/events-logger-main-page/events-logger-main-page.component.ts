a3087a749ce8fb8d2e4f896613399fbb10bde249
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-10-27 21:26:13 -0300

    [Feat] - infinity scroll works

--- libs/ui/master-page/src/lib/events-logger-main-page/src/lib/events-logger-main-page/events-logger-main-page.component.ts
+++ libs/ui/master-page/src/lib/events-logger-main-page/src/lib/events-logger-main-page/events-logger-main-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { EntryComponent } from 'src/app/Entry/entry.component';
+import { EntryInfiniteScrollComponent } from 'src/app/EntryInfiniteScroll/EntryInfiniteScroll/entry-infinite-scroll.component';
 
 @Component({
   selector: 'lib-events-logger-main-page',
   standalone: true,
-  imports: [CommonModule, RouterLink, EntryComponent],
+  imports: [CommonModule, RouterLink, EntryComponent, EntryInfiniteScrollComponent],
   templateUrl: './events-logger-main-page.component.html',
   styleUrls: ['./events-logger-main-page.component.css'],
 })
--- services/EntryInfiniteScroll/entry-infinity-scroll.service.ts
+++ services/EntryInfiniteScroll/entry-infinity-scroll.service.ts
@@ -7,18 +7,18 @@ import { delay, Observable, of } from 'rxjs';
 })
 export class EntryInfinityScrollService {
 
-  private totalItems=100;
+  private totalItems=500;
 
   getItems(page=1,itemsPerPage=10):Observable<string[]>{
-   const startIndex=(page-1)*itemsPerPage;
-   const endIndex=startIndex+itemsPerPage;
-   const items:string[]=[];
-   for(let i=startIndex;i<endIndex;i++){
-    if(i<this.totalItems){
-      items.push(`Item ${i+1}`);
+    const startIndex=(page-1)*itemsPerPage;
+    const endIndex=startIndex+itemsPerPage;
+    const items:string[]=[];
+    for(let i=startIndex;i<endIndex;i++){
+      if(i<this.totalItems){
+        items.push(`Item ${i+1}`);
+      }
     }
-   }
-   return of(items).pipe(delay(500));
+    return of(items).pipe(delay(500));
   }
 
   constructor() {}
